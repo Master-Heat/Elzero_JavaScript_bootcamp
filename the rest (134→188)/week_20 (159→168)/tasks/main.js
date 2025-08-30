45e0c29b0c919fc84522564e109494b21c1476ee
Master <masterheaterheting@gmail.com>
2025-08-30 14:43:15 +0300

    added learning files in week 20

---
+++ the rest (134→188)/week_20 (159→168)/tasks/main.js
@@ -0,0 +1,4 @@
+//! this is file related to task 8
+import * as modOne from "./mod-two.js";
+import calc from "./mod-one.js";
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
---
+++ the rest (134→188)/week_20 (159→168)/tasks/mod-one.js
@@ -0,0 +1,2 @@
+//! this is file related to task 8
+export default (num1, num2, num3) => num1 + num2 + num3;
